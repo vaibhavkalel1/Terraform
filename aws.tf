ea12df608f21940a1e3f74289ba9323661047e35
vaibhavkalel1 <110541903+vaibhavkalel1@users.noreply.github.com>
2024-02-29 16:05:28 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,1 +1,79 @@
+# main.tf
+
+# Define provider (AWS)
+provider "aws" {
+  region = "your_aws_region"
+}
+
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create subnet within the VPC
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+# Create internet gateway for internet access
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Create route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+# Associate subnet with the route table
+resource "aws_route_table_association" "my_subnet_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Create security group
+resource "aws_security_group" "instance_sg" {
+  name        = "instance_sg"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 instance
+resource "aws_instance" "example" {
+  ami             = "ami-02ca28e7c7b8f8be1"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.my_subnet.id
+  security_groups = [aws_security_group.instance_sg.name]
+
+  tags = {
+    Name = "Terraform Instance"
+  }
+}
 
